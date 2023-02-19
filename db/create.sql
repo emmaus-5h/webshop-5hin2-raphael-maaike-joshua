812f06b376e12956df97e9a6beb476219f81f190
Joshua Sedney <74650610+JoshuaSedney01@users.noreply.github.com>
2023-02-19 20:27:48 +0000

    tabel en kleur

--- db/create.sql
+++ db/create.sql
@@ -52,14 +52,14 @@ insert into products (name, description, code, price, categories_id) values ('Th
 insert into products (name, description, code, price) values ('Mario Kart 8 Deluxe', 'Dankzij de Nintendo Switch kunnen de ultieme versie van mario Kart 8 altijd en overal spelen. Er is zelf een lokale, draadloze multiplayerstand voor maximaal acht spelers! .', '077030122-3', 51.99);
 insert into products (name, description, code, price) values ('Pokemon Scarlet', 'Ontdek nieuwe Pokemon in een grenzeloos openwereldavontuur in Pokemon Scarlet op de Nintendo Switch.', '445924201-X', 60.00);
 insert into products (name, description, code, price) values ('Aninmal crossing: New horizons', 'In Animal Crossing ontdek je iedere dag nieuwe vrienden en verrassingen. Of je nu de burgemeester bent, huizen ontwerpt of gewoon een kijkje wilt nemen, de wereld van Animal Crossing is altijd een leuke en plezierige plek.', '693155505-7', 49.99);
-insert into products (name, description, code, price) values ('Super Mario Party', 'Nulla ut erat id mauris vulputate elementum. Nullam varius. Nulla facilisi.', '686928463-6', 49.99);
-insert into products (name, description, code, price) values ('Kirby Star Allies', 'Nam ultrices, libero non mattis pulvinar, nulla pede ullamcorper augue, a suscipit nulla elit ac nulla.', '492662523-7', 54.99);
-insert into products (name, description, code, price) values ('Super Mario Maker 2', 'Nam ultrices, libero non mattis pulvinar, nulla pede ullamcorper augue, a suscipit nulla elit ac nulla.', '492662523-7', 52.00);
-insert into products (name, description, code, price) values ('Splatoon', 'Nam ultrices, libero non mattis pulvinar, nulla pede ullamcorper augue, a suscipit nulla elit ac nulla.', '492662523-7', 50.99);
-insert into products (name, description, code, price) values ('Wii Party U', 'Nam ultrices, libero non mattis pulvinar, nulla pede ullamcorper augue, a suscipit nulla elit ac nulla.', '492662523-7', 36.99);
-insert into products (name, description, code, price) values ('Pokemon Black 2', 'Nam ultrices, libero non mattis pulvinar, nulla pede ullamcorper augue, a suscipit nulla elit ac nulla.', '492662523-7', 104.99);
-insert into products (name, description, code, price) values ('Sonic Generations', 'Nam ultrices, libero non mattis pulvinar, nulla pede ullamcorper augue, a suscipit nulla elit ac nulla.', '492662523-7', 39.99);
-insert into products (name, description, code, price) values ('Yokai Watch 2 Gigageesten', 'Nam ultrices, libero non mattis pulvinar, nulla pede ullamcorper augue, a suscipit nulla elit ac nulla.', '492662523-7', 50.99);
+insert into products (name, description, code, price) values ('Super Mario Party', 'Geef altijd, overal en met iedereen een feestje in Super Mario Party.', '686928463-6', 49.99);
+insert into products (name, description, code, price) values ('Kirby Star Allies', 'Samen met je vrienden sta je sterk in Krby Star Allies.', '492662523-7', 54.99);
+insert into products (name, description, code, price) values ('Super Mario Maker 2', 'Het aantal Levels in super mario maker is vrijwel onbeperkt. daarom biedt deze game je eindeloos mario-plezier.', '492662523-7', 52.00);
+insert into products (name, description, code, price) values ('Splatoon', 'ga kliederen in teams van vier in Splatoon .', '492662523-7', 50.99);
+insert into products (name, description, code, price) values ('Wii Party U', 'Vecht, dans en race tegen je vrienden op Wii Party.', '492662523-7', 36.99);
+insert into products (name, description, code, price) values ('Pokemon Black 2', 'Het nieuwe pokemon in de Unova regio gaat van start ga samen met je vrienden op reis en versla de champion.', '492662523-7', 104.99);
+insert into products (name, description, code, price) values ('Sonic Generations', 'Help sonic en zijn vriend van de kwaadardige dokter Eggman.', '492662523-7', 39.99);
+insert into products (name, description, code, price) values ('Yokai Watch 2 Gigageesten', 'Er staan missies, nieuwe locaties en nog veel meer te wachten als de ondeugende Yo-kai Wicked worden.', '492662523-7', 50.99);
 
 /* Consoles */
 INSERT INTO consoles (name, description, code, price) values ('Nintendo Switch - OLED Model', 'Get the gaming system that lets you play the games you want, wherever you are. However you like. Bring the full home gaming system experience with you to the park, on an airplane, in a car, or to the apartment of a friend.', '0045496883386', 349.99);
@@ -107,3 +107,23 @@ INSERT INTO categories (name) values ("RPG");
 INSERT INTO categories (name) values ("MMO");
 INSERT INTO categories (name) values ("Adventure");
 INSERT INTO categories (name) values ("Fantasy");
+
+/* reviews */
+INSERT INTO reviews (name,review,datum) values  ("The Legend of Zelda"," Awesome open world game. Great story, nice atmosphere and so much to do.","11 december 2022"); 
+INSERT INTO reviews (name,review,datum) values  ("Mario Kart 8 Deluxe","Much better than the orginal. It has more diversity.","1 july 2022"); 
+
+INSERT INTO reviews (name,review,datum) values  ("Pokemon Scarlet"," The best open world Pokemon game. It has some glitches but and bugs but still great job. ","11 april 2022"); 
+INSERT INTO reviews (name,review,datum) values  ("Animal crossing: new horizons"," It's so calming I loved it. I hated the beginning tho.","6 may 2021"); 
+INSERT INTO reviews (name,review,datum) values  ("Super Mario Party"," Great to play with friends, very easy to play it every where."," 23 february 2021"); 
+INSERT INTO reviews (name,review,datum) values  ("Splatoon"," Fun game","2 june 2020"); 
+INSERT INTO reviews (name,review,datum) values  ("Kirby Star Allies"," I LOVE KIRBY, he is so cute. ","25 july 2021"); 
+INSERT INTO reviews (name,review,datum) values  ("Super Mario Maker 2","iI made a level so hard that nobody can finish."," 5 august 2022"); 
+INSERT INTO reviews (name,review,datum) values  ("Wii Party U"," I love and hate this game.","15 september 2021"); 
+INSERT INTO reviews (name,review,datum) values  ("Pokemon Black 2","This game was so hard to play. AND WHY WAS CYNTHIA IN THE GAME.","25 december 2022"); 
+INSERT INTO reviews (name,review,datum) values  ("Sonic Generations"," I haven't played sonic in a while so it was fun to play it again"," 15 october 2015"); 
+INSERT INTO reviews (name,review,datum) values  ("Yokai Watch 2 Gigageesten","The main story is so good, the characters are so cute and the bosses are hard. Great game","14 february 20223"); 
+INSERT INTO reviews (name,review,datum) values  ("Nintendo Switch - OLED Model","i don't know why I bought this model, it was the same as the first model ","2 august 2018"); 
+INSERT INTO reviews (name,review,datum) values  ("Nintendo Switch","My siblings and I started bonding more because of this.","21 march 2020"); 
+INSERT INTO reviews (name,review,datum) values  ("Nintendo Switch lite"," Its great to play it if you don't have anyone to play with","17 november 2017"); 
+INSERT INTO reviews (name,review,datum) values  ("Nintendo 3DS","Its so cool to see some games in 3D.","2 june 2014"); 
+INSERT INTO reviews (name,review,datum) values  ("Nintendo Wii U Edition"," Cool to play with my friends at home","3 july 2012"); 
